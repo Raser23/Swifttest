7f6b786c685198677cf1ea79e14d4e6341c69dda
Neshchadim Sergey <ser.neshadim@yandex.ru>
2017-08-10 12:31:00 +0700

    Second commit

---
+++ Listing 8.8.swift
@@ -0,0 +1,15 @@
+//
+//  Listing 8.8.swift
+//  Test
+//
+//  Created by Сергей Нещадим on 10/08/2017.
+//  Copyright © 2017 Нещадим Сергей. All rights reserved.
+//
+
+import Foundation
+
+// константы
+let a = 1
+let b = 2
+// сравнение значений констант
+a <= b ? print("A меньше или равно B"):print("A больше B")
---
+++ Test/Arrays.swift
@@ -0,0 +1,12 @@
+//
+//  Arrays.swift
+//  Test
+//
+//  Created by Сергей Нещадим on 10/08/2017.
+//  Copyright © 2017 Нещадим Сергей. All rights reserved.
+//
+
+import Foundation
+
+var ar = [1,2,3,4]
+print(ar)
