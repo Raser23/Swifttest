72228ce0e9ee230f36d288d8dc554f20233883ac
Neshchadim Sergey <ser.neshadim@yandex.ru>
2017-08-10 11:52:55 +0700

    Initial Commit

---
+++ Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by Сергей Нещадим on 10/08/2017.
+//  Copyright © 2017 Нещадим Сергей. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
